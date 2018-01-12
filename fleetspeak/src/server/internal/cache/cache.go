ac52cb8835c4eda9966a6b496873dcc561b56b7a
Ben Galehouse <bgalehouse@gmail.com>
2018-01-12 11:47:05 +0100

    Tweak comments related to new cache package. (#29)

--- fleetspeak/src/server/internal/cache/cache.go
+++ fleetspeak/src/server/internal/cache/cache.go
@@ -1,3 +1,19 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package cache contains caching structures using internally by the Fleetspeak
+// server.
 package cache
 
 import (
@@ -9,8 +25,8 @@ import (
 )
 
 var (
-	// How long client data should be considered valid for. Variable to support
-	// unit testing.
+	// MaxAge defines how long client data should be considered valid
+	// for. Variable to support unit testing.
 	MaxAge = 30 * time.Second
 
 	// We occasionally expunge old client data records, to be tidy
