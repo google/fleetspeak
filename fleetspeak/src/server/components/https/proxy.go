a15886839cca4f83bf6a4bad02e1b880300168e3
Ben Galehouse <bgalehouse@google.com>
2019-04-02 14:13:03 +0200

    Add copyright blocks and a few high level comments.

--- fleetspeak/src/server/components/https/proxy.go
+++ fleetspeak/src/server/components/https/proxy.go
@@ -1,3 +1,19 @@
+// Copyright 2019 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package https provides generic implementations and utility methods for
+// Fleetspeak https communication component type.
 package https
 
 import (
@@ -10,6 +26,11 @@ import (
 	proxyproto "github.com/pires/go-proxyproto"
 )
 
+// ProxyListener wraps a net.Listener and adapts the perceived source of the connection
+// to be that provided by the PROXY protocol header string. Should be used only when the
+// server is behind a load balancer which implements the PROXY protocol.
+//
+// https://www.haproxy.org/download/1.8/doc/proxy-protocol.txt
 type ProxyListener struct {
 	net.Listener
 }
