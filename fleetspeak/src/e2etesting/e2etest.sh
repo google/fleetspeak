f5be7a1370c82928cdcdd160bbc1b1c2f7393b7d
Alexandr-TS <alexandr@tsaplin.ru>
2020-07-07 09:03:06 +0500

    Remove installing unused pip packages

--- fleetspeak/src/e2etesting/e2etest.sh
+++ fleetspeak/src/e2etesting/e2etest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd $(dirname $0)/../../../
 python3 -m venv /tmp/.venv/FLEETSPEAK
-/tmp/.venv/FLEETSPEAK/bin/pip install absl-py protobuf fleetspeak > /dev/null
+/tmp/.venv/FLEETSPEAK/bin/pip install fleetspeak > /dev/null
 source /tmp/.venv/FLEETSPEAK/bin/activate
 pip install -e frr_python
 go run fleetspeak/src/e2etesting/run_end_to_end_tests.go --mysql_database=fleetspeak_test --mysql_username=fsuser --mysql_password=fsuserPass1!
