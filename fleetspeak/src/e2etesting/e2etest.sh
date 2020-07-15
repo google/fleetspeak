6cfe06de8d5a932b817caf65f25a71cee45faf22
Alexandr-TS <alexandr@tsaplin.ru>
2020-07-15 11:59:47 +0500

    Update e2etest.sh

--- fleetspeak/src/e2etesting/e2etest.sh
+++ fleetspeak/src/e2etesting/e2etest.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 cd $(dirname $0)/../../../
-python3 -m venv /tmp/.venv/FLEETSPEAK
-source /tmp/.venv/FLEETSPEAK/bin/activate
-pip install -e frr_python
+pip3 install -e frr_python
 go run fleetspeak/src/e2etesting/run_end_to_end_tests.go --mysql_address=127.0.0.1:3306 --mysql_database=fleetspeak_test --mysql_username=fsuser --mysql_password=fsuserPass1! --num_clients=1 --num_servers=1
