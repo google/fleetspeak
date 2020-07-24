eefd5d5fcbd214900380e146f7cbf6a21c9ae711
Alexandr-TS <alexandr@tsaplin.ru>
2020-07-24 11:55:22 +0500

    Refactor terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,22 @@
+variable "project_name" {
+    type = string
+}
+
+variable "num_clients" {
+    type = number
+    default = 1
+}
+
+variable "num_servers" {
+    type = number
+    default = 1
+}
+
+
 provider "google" {
     version = "3.5.0"
 
-	credentials = file("/home/atsaplin/.config/gcloud/application_default_credentials.json")
-
-    project = "fs-internship"
+    project = var.project_name
     region = "europe-west1"
     zone = "europe-west1-b"
 }
@@ -13,98 +26,44 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_firewall" "allow-ssh" {
-	name = "allow-ssh"
-	network = google_compute_network.vpc_network.self_link
+    name = "allow-ssh"
+    network = google_compute_network.vpc_network.self_link
 
-	allow {
-		protocol = "tcp"
-		ports = ["22"]
-	}
+    allow {
+        protocol = "tcp"
+        ports = ["22"]
+    }
 
-	source_ranges = ["0.0.0.0/0"]
-	target_tags = ["ssh"]
-}
-
-variable "num_clients" {
-	type = number
-	default = 1
-}
-
-variable "num_servers" {
-	type = number
-	default = 1
+    source_ranges = ["0.0.0.0/0"]
+    target_tags = ["ssh"]
 }
 
 locals {
-	ip_address_ranges_prefix = "10.132.0"
-	ip_address_ranges = format("%s.0/20", local.ip_address_ranges_prefix)
-	main_vm_host = cidrhost(local.ip_address_ranges, 10)
-	master_server_host = cidrhost(local.ip_address_ranges, 11)
-	first_fs_server_host_suffix = 12
-	fs_admin_host = cidrhost(local.ip_address_ranges, local.first_fs_server_host_suffix)
-	first_fs_client_host_suffix = local.first_fs_server_host_suffix + var.num_servers
-}
-
-data "template_file" "master_server_install" {
-	template = file("master_server_start.sh")
-	vars = {
-		storage_bucket_url = google_storage_bucket.common-files-store.url	
-		admin_host = local.fs_admin_host
-		master_server_host = local.master_server_host
-	}
-}
-
-data "template_file" "fs_server_install" {
-	count = var.num_servers
-	template = file("fs_server_start.sh")
-	vars = {
-		mysql_instance_connection_name = google_sql_database_instance.fs-db.connection_name
-		storage_bucket_url = google_storage_bucket.common-files-store.url	
-		master_server_host = local.master_server_host
-		self_host = cidrhost(local.ip_address_ranges, local.first_fs_server_host_suffix + count.index)
-		self_index = count.index
-	}
-}
-
-data "template_file" "fs_client_install" {
-	count = var.num_clients
-	template = file("fs_client_start.sh")
-	vars = {
-		storage_bucket_url = google_storage_bucket.common-files-store.url	
-		self_index = count.index
-	}
-}
-
-data "template_file" "main_vm_install" {
-	template = file("main_vm_start.sh")
-	vars = {
-		mysql_instance_connection_name = google_sql_database_instance.fs-db.connection_name
-		storage_bucket_url = google_storage_bucket.common-files-store.url	
-		admin_host = local.fs_admin_host
-		master_server_host = local.master_server_host
-		ip_address_ranges_prefix = local.ip_address_ranges_prefix
-		first_fs_server_host_suffix = local.first_fs_server_host_suffix  
-		num_clients = var.num_clients
-		num_servers = var.num_servers
-	}
+    ip_address_ranges_prefix = "10.132.0"
+    ip_address_ranges = format("%s.0/20", local.ip_address_ranges_prefix)
+    main_vm_host = cidrhost(local.ip_address_ranges, 10)
+    master_server_host = cidrhost(local.ip_address_ranges, 11)
+    first_fs_server_host_suffix = 12
+    fs_admin_host = cidrhost(local.ip_address_ranges, local.first_fs_server_host_suffix)
+    first_fs_client_host_suffix = local.first_fs_server_host_suffix + var.num_servers
 }
 
 resource "random_id" "bucket_name_suffix" {
-	byte_length = 4
+    byte_length = 4
 }
 
 resource "google_storage_bucket" "common-files-store" {
     name = "common-files-store-${random_id.bucket_name_suffix.hex}"
-	location = "EU"
-	force_destroy = true
-	bucket_policy_only = true
+    location = "EU"
+    force_destroy = true
+    bucket_policy_only = true
 }
 
 resource "google_compute_instance" "vm_instance" {
     name = "terraform-instance"
     machine_type = "n1-standard-1"
 
-	tags = ["ssh"]
+    tags = ["ssh"]
 
     boot_disk {
         initialize_params {
@@ -112,28 +71,41 @@ resource "google_compute_instance" "vm_instance" {
         }
     }
 
-	depends_on = [
+    depends_on = [
         google_compute_network.vpc_network,
-	]
-		
+    ]
+
     network_interface {
         network = google_compute_network.vpc_network.self_link
-		network_ip = local.main_vm_host
+        network_ip = local.main_vm_host
         access_config {
         }
     }
 
-	metadata_startup_script = data.template_file.main_vm_install.rendered
-	service_account {
-		scopes = ["cloud-platform"]
-	}
+    metadata_startup_script = templatefile(
+        "main_vm_start.sh",
+        {
+            mysql_instance_connection_name = google_sql_database_instance.fs-db.connection_name
+            storage_bucket_url = google_storage_bucket.common-files-store.url
+            admin_host = local.fs_admin_host
+            master_server_host = local.master_server_host
+            ip_address_ranges_prefix = local.ip_address_ranges_prefix
+            first_fs_server_host_suffix = local.first_fs_server_host_suffix
+            num_clients = var.num_clients
+            num_servers = var.num_servers
+        }
+    )
+
+    service_account {
+        scopes = ["cloud-platform"]
+    }
 }
 
 resource "google_compute_instance" "master_server_instance" {
     name = "master-server-instance"
     machine_type = "n1-standard-1"
 
-	tags = ["ssh"]
+    tags = ["ssh"]
 
     boot_disk {
         initialize_params {
@@ -141,29 +113,37 @@ resource "google_compute_instance" "master_server_instance" {
         }
     }
 
-	depends_on = [
+    depends_on = [
         google_compute_network.vpc_network,
-	]
-		
+    ]
+
     network_interface {
         network = google_compute_network.vpc_network.self_link
-		network_ip = local.master_server_host
+        network_ip = local.master_server_host
         access_config {
         }
     }
 
-	metadata_startup_script = data.template_file.master_server_install.rendered
-	service_account {
-		scopes = ["cloud-platform"]
-	}
+    metadata_startup_script = templatefile(
+        "master_server_start.sh",
+        {
+            storage_bucket_url = google_storage_bucket.common-files-store.url
+            admin_host = local.fs_admin_host
+            master_server_host = local.master_server_host
+        }
+    )
+
+    service_account {
+        scopes = ["cloud-platform"]
+    }
 }
 
 resource "google_compute_instance" "fs_server_instance" {
-	count = var.num_servers
+    count = var.num_servers
     name = "fs-server-instance-${count.index}"
     machine_type = "n1-standard-1"
 
-	tags = ["ssh"]
+    tags = ["ssh"]
 
     boot_disk {
         initialize_params {
@@ -171,29 +151,39 @@ resource "google_compute_instance" "fs_server_instance" {
         }
     }
 
-	depends_on = [
+    depends_on = [
         google_compute_network.vpc_network,
-	]
-		
+    ]
+
     network_interface {
         network = google_compute_network.vpc_network.self_link
-		network_ip = cidrhost(local.ip_address_ranges, local.first_fs_server_host_suffix + count.index)
+        network_ip = cidrhost(local.ip_address_ranges, local.first_fs_server_host_suffix + count.index)
         access_config {
         }
     }
 
-	metadata_startup_script = element(data.template_file.fs_server_install.*.rendered, count.index)
-	service_account {
-		scopes = ["cloud-platform"]
-	}
+    metadata_startup_script = templatefile(
+        "fs_server_start.sh",
+        {
+            mysql_instance_connection_name = google_sql_database_instance.fs-db.connection_name
+            storage_bucket_url = google_storage_bucket.common-files-store.url
+            master_server_host = local.master_server_host
+            self_host = cidrhost(local.ip_address_ranges, local.first_fs_server_host_suffix + count.index)
+            self_index = count.index
+        }
+    )
+
+    service_account {
+        scopes = ["cloud-platform"]
+    }
 }
 
 resource "google_compute_instance" "fs_client_instance" {
-	count = var.num_clients
+    count = var.num_clients
     name = "fs-client-instance-${count.index}"
     machine_type = "n1-standard-1"
 
-	tags = ["ssh"]
+    tags = ["ssh"]
 
     boot_disk {
         initialize_params {
@@ -201,25 +191,32 @@ resource "google_compute_instance" "fs_client_instance" {
         }
     }
 
-	depends_on = [
+    depends_on = [
         google_compute_network.vpc_network,
-	]
-		
+    ]
+
     network_interface {
         network = google_compute_network.vpc_network.self_link
-		network_ip = cidrhost(local.ip_address_ranges, local.first_fs_client_host_suffix + count.index)
+        network_ip = cidrhost(local.ip_address_ranges, local.first_fs_client_host_suffix + count.index)
         access_config {
         }
     }
 
-	metadata_startup_script = element(data.template_file.fs_client_install.*.rendered, count.index)
-	service_account {
-		scopes = ["cloud-platform"]
-	}
+    metadata_startup_script = templatefile(
+        "fs_client_start.sh",
+        {
+            storage_bucket_url = google_storage_bucket.common-files-store.url
+            self_index = count.index
+        }
+    )
+
+    service_account {
+        scopes = ["cloud-platform"]
+    }
 }
 
 resource "random_id" "db_name_suffix" {
-	byte_length = 4
+    byte_length = 4
 }
 
 resource "google_sql_database_instance" "fs-db" {
@@ -229,21 +226,21 @@ resource "google_sql_database_instance" "fs-db" {
     settings {
         tier = "db-n1-standard-1"
 
-		database_flags {
-		    name = "max_allowed_packet"
-		    value = "1073741824"
-		}
+        database_flags {
+            name = "max_allowed_packet"
+            value = "1073741824"
+        }
 
-		database_flags {
-		    name = "log_output"
-		    value = "FILE"
-		}
+        database_flags {
+            name = "log_output"
+            value = "FILE"
+        }
 
-		database_flags {
-		    name = "slow_query_log"
-		    value = "on"
-		}
-	}
+        database_flags {
+            name = "slow_query_log"
+            value = "on"
+        }
+    }
 }
 
 resource "google_sql_user" "users" {
@@ -260,5 +257,5 @@ resource "google_sql_database" "fs-db" {
 }
 
 output "mysql_host" {
-	value = google_sql_database_instance.fs-db.ip_address[0].ip_address
+    value = google_sql_database_instance.fs-db.ip_address[0].ip_address
 }
