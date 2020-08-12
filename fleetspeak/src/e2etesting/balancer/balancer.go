85e7c017bd956dcf32831cf6741b06ce4c3c26c9
Alexandr Tsaplin <33631074+Alexandr-TS@users.noreply.github.com>
2020-08-12 20:40:24 +0500

    Add Load balancer to local FS installation for e2e testing (#274)
    * Add load balancer for local FS e2e testing
    
    * go fmt
    
    * Add error for exceeded number of retries
    
    * Fix BuildConfigurations() for cloud version

---
+++ fleetspeak/src/e2etesting/balancer/balancer.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	serversFile        = flag.String("servers_file", "", "File with server hosts")
+	serverFrontendAddr = flag.String("frontend_address", "", "Frontend address for clients to connect")
+)
+
+func copy(wc io.WriteCloser, r io.Reader) {
+	defer wc.Close()
+	io.Copy(wc, r)
+}
+
+func run() error {
+	dat, err := ioutil.ReadFile(*serversFile)
+	if err != nil {
+		return fmt.Errorf("Failed to read serversFile: %v", err)
+	}
+	serverHosts := strings.Fields(string(dat))
+	if len(serverHosts) == 0 {
+		return fmt.Errorf("No server hosts were provided")
+	}
+
+	ln, err := net.Listen("tcp", *serverFrontendAddr)
+	if err != nil {
+		return fmt.Errorf("Failed to bind: %v", err)
+	}
+	log.Printf("Load balancer started on %v\n", *serverFrontendAddr)
+
+	for {
+		lbConn, err := ln.Accept()
+		if err != nil {
+			return fmt.Errorf("Failed to accept connection: %v", err)
+		}
+		var serverAddr string
+		var serverConn net.Conn
+		retriesLeft := 10
+		for {
+			serverAddr = serverHosts[rand.Int()%len(serverHosts)]
+			serverConn, err = net.Dial("tcp", serverAddr)
+			if err != nil {
+				log.Printf("Failed to connect to server (%v): %v, retrying...\n", serverAddr, err)
+				retriesLeft--
+				if retriesLeft < 0 {
+					return fmt.Errorf("Maximum number of retries exceeded - no active servers were found")
+				}
+				time.Sleep(time.Second * 2)
+			} else {
+				break
+			}
+		}
+		log.Printf("Connection accepted, server: %v\n", serverAddr)
+		go copy(serverConn, lbConn)
+		go copy(lbConn, serverConn)
+	}
+}
+
+func main() {
+	flag.Parse()
+	err := run()
+	if err != nil {
+		fmt.Printf("Load balancer failed: %v\n", err)
+		os.Exit(1)
+	}
+}
