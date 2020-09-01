1db501bd1c1ad790430338b8f25572714ff63765
mol123 <60134413+mol123@users.noreply.github.com>
2020-09-01 21:42:33 +0200

    In balancer, move go-proxyproto related code to its own file. (#289)
    * In balancer, move go-proxyproto related code to its own file.
    
    * Remove noproxyproto.

---
+++ fleetspeak/src/e2etesting/balancer/proxyproto/proxyproto.go
@@ -0,0 +1,49 @@
+package proxyproto
+
+import (
+	"fmt"
+	proxyproto "github.com/pires/go-proxyproto"
+	"io"
+	"net"
+	"strconv"
+)
+
+func splitHostPort(addr string) (net.IP, uint16, error) {
+	hostStr, portStr, err := net.SplitHostPort(addr)
+	if err != nil {
+		return nil, 0, err
+	}
+	host := net.ParseIP(hostStr)
+	if host == nil {
+		return nil, 0, fmt.Errorf("Failed to parse IP")
+	}
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		return nil, 0, fmt.Errorf("Failed to parse port: %v", err)
+	}
+	return host, uint16(port), nil
+}
+
+func WriteFirstProxyMessage(w io.Writer, srcAddr, dstAddr string) error {
+	srcHost, srcPort, err := splitHostPort(srcAddr)
+	if err != nil {
+		return fmt.Errorf("Failed to parse source (client) address: %v", err)
+	}
+	dstHost, dstPort, err := splitHostPort(dstAddr)
+	if err != nil {
+		return fmt.Errorf("Failed to parse destination (server) address: %v", err)
+	}
+	header := proxyproto.Header{
+		Version:            1,
+		TransportProtocol:  proxyproto.TCPv4,
+		SourceAddress:      srcHost,
+		DestinationAddress: dstHost,
+		SourcePort:         srcPort,
+		DestinationPort:    dstPort,
+	}
+	_, err = header.WriteTo(w)
+	if err != nil {
+		return fmt.Errorf("Failed to write Proxy header: %v", err)
+	}
+	return nil
+}
