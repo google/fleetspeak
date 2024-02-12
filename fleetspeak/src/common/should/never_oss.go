91aad1c4ab4d7fc89b12ca4c49dc1cfee7b805a6
Günther Noack <gnoack@google.com>
2024-02-12 07:37:32 -0800

    Make it possible to signal "impossible" conditions
    This is a lightweight way to experimentally probe for dead code, or code which
    should be very seldomly run.  For example, one might add:
    
        should.Never("timeout during operation XYZ")
    
    and then after deployment we should be able to monitor how often this happened.
    
    The open source version has an empty implementation.  The Google-internal
    version increments a counter which lets us observe that.  The right variant is
    picked at compile time through build constraints.
    
    PiperOrigin-RevId: 606247072

---
+++ fleetspeak/src/common/should/never_oss.go
@@ -0,0 +1,29 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build oss
+
+// Package should lets callers indicate impossible conditions from the code.
+//
+// It is a lightweight way to make these impossible conditions observable,
+// when it is hooked up with an appropriate monitoring framework.
+package should
+
+// Never indicates that the given condition should never (or seldomly) happen.
+//
+// Callers should only pass a fixed set of strings to Never(),
+// to avoid a state explosion in monitoring counters.
+func Never(condition string) {
+	// Unfortunately empty in the open source version.
+}
